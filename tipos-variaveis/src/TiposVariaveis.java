36d38cd9adef15fed13d8ecdef6af346371060b9
Fspaulohenrique <fspaulohenrique@hotmail.com>
2024-09-17 21:25:54 -0300

    Entendendo as variaveis e a constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome ="Paulo Henrique";
+
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Constantes 
+        final double VALOR_DE_P = 3.14;I
+
     }
 }
